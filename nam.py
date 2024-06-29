f138452d159c086e22507233682f1f8ff4e54290
Nguyennam2201 <nguyennam22012008@gmail.com>
2024-06-29 23:49:24 +0700

    Add files via upload

--- nam.py
+++ nam.py
@@ -52,6 +52,7 @@ print('\x1b[38;5;207m╚══\x1b[38;5;226m══\x1b[38;5;99m══\x1b[38;5;4
 print('\x1b[38;5;226m[1] \x1b[38;5;46mTOOL \x1b[38;5;51mTDS TIKTOK + TIKTOK NOW')
 print('\x1b[38;5;226m[2] \x1b[38;5;46mTOOL \x1b[38;5;51mTDS BẰNG PAGE PRO5')
 print('\x1b[38;5;226m[3] \x1b[38;5;46mTOOL \x1b[38;5;51mTDS FACEBOOK FULL JOD')
+print('\x1b[38;5;226m[4] \x1b[38;5;46mTOOL \x1b[38;5;51mLẤY MAIL TẠO NICK FB')
 print('\x1b[38;5;46m-----------------------------------------------------------------')
 import requests
 
@@ -61,6 +62,7 @@ urls = [
     'https://raw.githubusercontent.com/Nguyennam2201/Nguyennam2201/main/tdstiktok.py',
     'https://raw.githubusercontent.com/Nguyennam2201/Nguyennam2201/main/tdspage.py',
     'https://raw.githubusercontent.com/Nguyennam2201/Nguyennam2201/main/tds.py',
+    'https://raw.githubusercontent.com/Nguyennam2201/Nguyennam2201/main/mail.py',
 ]
 
 if 1 <= chon <= 3:
