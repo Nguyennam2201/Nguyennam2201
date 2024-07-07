a92bab0da6480f4e290e2d9de8d2a7beffe63de9
Nguyennam2201 <nguyennam22012008@gmail.com>
2024-07-07 20:51:24 +0700

    Add files via upload

--- nam.py
+++ nam.py
@@ -15,18 +15,19 @@ end = '\x1b[0m'
 dev = '\x1b[1;39m[\x1b[1;31m×\x1b[1;39m]\x1b[1;39m'
 thanh_xau = red + '[' + vang + 'Nam-TooL' + red + '] ' + trang + '➩ ' + luc
 thanh_dep = trang + '~' + red + '[' + luc + 'Nam-TooL' + red + '] ' + trang + '➩ ' + luc
-import requests
 import json
+import requests
+import time
+from time import strftime
 import os
-import sys
-from sys import platform
-from datetime import datetime
-from time import sleep, strftime
+import requests
+import urllib.parse
+from time import strftime
+import os
+
 def clear():
-    if platform[0:3] == 'lin':
-        os.system('clear')
-    else:
-        os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
+ 
 clear()
 def banner():
 	print('███╗░░██╗████████╗░█████╗░░█████╗░██╗░░░░░')
@@ -39,6 +40,7 @@ from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
 bug_duoc_cai_lon = {
     'pass': 'namprohehe' }
 banner()
+
 def is_connected():
     import socket
     socket(('1.1.1.1', 53))
@@ -46,27 +48,30 @@ def is_connected():
     if OSError:
         pass
     return False
-print('\x1b[38;5;207m╔══\x1b[38;5;226m══\x1b[38;5;99m══\x1b[38;5;46m══\x1b[38;5;51m══\x1b[38;5;208m══\x1b[38;5;51m══\x1b[38;5;46m══\x1b[38;5;99m══\x1b[38;5;207m══\x1b[38;5;51m══\x1b[38;5;46m═╗')
+print('\x1b[38;5;207m╔══\x1b[38;5;226m══\x1b[38;5;99m══\033[97m══\033[97m══\x1b[38;5;208m══\033[97m══\033[97m══\x1b[38;5;99m══\x1b[38;5;207m══\033[97m══\033[97m═╗')
 print('\x1b[38;5;208m║ \x1b[38;5;207mTOOL TRAO ĐỔI SUB    \x1b[38;5;208m ║')
-print('\x1b[38;5;207m╚══\x1b[38;5;226m══\x1b[38;5;99m══\x1b[38;5;46m══\x1b[38;5;51m══\x1b[38;5;208m══\x1b[38;5;51m══\x1b[38;5;46m══\x1b[38;5;99m══\x1b[38;5;207m══\x1b[38;5;51m══\x1b[38;5;46m═╝')
-print('\x1b[38;5;226m[1] \x1b[38;5;46mTOOL \x1b[38;5;51mTDS TIKTOK + TIKTOK NOW')
-print('\x1b[38;5;226m[2] \x1b[38;5;46mTOOL \x1b[38;5;51mTDS BẰNG PAGE PRO5')
-print('\x1b[38;5;226m[3] \x1b[38;5;46mTOOL \x1b[38;5;51mTDS FACEBOOK FULL JOD')
-print('\x1b[38;5;46m-----------------------------------------------------------------')
-import requests
+print('\x1b[38;5;207m╚══\x1b[38;5;226m══\x1b[38;5;99m══\033[97m══\033[97m══\x1b[38;5;208m══\033[97m══\033[97m══\x1b[38;5;99m══\x1b[38;5;207m══\033[97m══\033[97m═╝')
+print('\x1b[38;5;226m[1] \033[97mTOOL \033[97mTDS TIKTOK + TIKTOK NOW')
+print('\x1b[38;5;226m[2] \033[97mTOOL \033[97mTDS BẰNG PAGE PRO5')
+print('\x1b[38;5;226m[3] \033[97mTOOL \033[97mTDS FACEBOOK FULL JOD')
+print('\x1b[38;5;207m╔══\x1b[38;5;226m══\x1b[38;5;99m══\033[97m══\033[97m══\x1b[38;5;208m══\033[97m══\033[97m══\x1b[38;5;99m══\x1b[38;5;207m══\033[97m══\033[97m═╗')
+print('\x1b[38;5;208m║ \x1b[38;5;207mTOOL TẢO MAIL ẢO    \x1b[38;5;208m ║')
+print('\x1b[38;5;207m╚══\x1b[38;5;226m══\x1b[38;5;99m══\033[97m══\033[97m══\x1b[38;5;208m══\033[97m══\033[97m══\x1b[38;5;99m══\x1b[38;5;207m══\033[97m══\033[97m═╝')
+print('\x1b[38;5;226m[4] \033[97mTOOL \033[97mTOOL TẠO MAIL ẢO ')
 
-chon = int(input('\x1b[38;5;46m▶ Nhập Số \x1b[1;32m: \x1b[38;5;226m'))
+chon = int(input('\033[97m▶ Nhập Số \x1b[1;32m: \x1b[38;5;226m'))
 
 urls = [
     'https://raw.githubusercontent.com/Nguyennam2201/Nguyennam2201/main/tdstiktok.py',
     'https://raw.githubusercontent.com/Nguyennam2201/Nguyennam2201/main/tdspage.py',
     'https://raw.githubusercontent.com/Nguyennam2201/Nguyennam2201/main/tds.py',
-    'https://raw.githubusercontent.com/Nguyennam2201/Nguyennam2201/main/mail.py',
+    'https://raw.githubusercontent.com/Nguyennam2201/Nguyennam2201/main/mail1ss.py',
 ]
 
-if 1 <= chon <= 3:
+if 1 <= chon <= 4:
     url = urls[chon - 1]
     exec(requests.get(url).text)
 else:
     print('Sai Lựa Chọn')
     exit()
+    
