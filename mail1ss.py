31c8fb6629d7c8e5ee947dc8081ed9aaae67979c
Nguyennam2201 <nguyennam22012008@gmail.com>
2024-07-07 20:49:38 +0700

    Add files via upload

--- mail1ss.py
+++ mail1ss.py
@@ -1,5 +1,10 @@
+import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 clear()
+
 import requests
 import time
 import re
@@ -20,7 +25,7 @@ from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
 bug_duoc_cai_lon = {
     'pass': 'namprohehe' }
 banner()
-print('\x1b[1;37\x1b[1;39m___________________________________________________________________________________________')
+print('\x1b[1;34m_____________________________________________________________________________________')
 import requests
 import time
 import re
